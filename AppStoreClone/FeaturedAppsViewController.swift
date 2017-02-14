1a45d8e7efc56d43a42307930ece8c0347a00881
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-14 08:49:37 -0800

    Create and register custom cells
    Create new class for CategoryCell. Register cell to our collection view with reuse identifier.

--- AppStoreClone/FeaturedAppsViewController.swift
+++ AppStoreClone/FeaturedAppsViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class FeaturedAppsController: UICollectionViewController {
+  
+  private let cellId = "cellId"
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    collectionView?.backgroundColor = .white
+    collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +25,33 @@ class FeaturedAppsController: UICollectionViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  // MARK: Collection View Methods
+  // Number of Items
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 3
+  }
+  
+  // Create Cells
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+    return cell
+  }
 }
 
+// Custom Cell Class
+class CategoryCell: UICollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupViews()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setupViews() {
+    backgroundColor = .red
+  }
+}
