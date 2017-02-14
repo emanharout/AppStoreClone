3dbf1abc981cc565b16964e4aaee3dd17a9bcc07
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-14 09:41:04 -0800

    Setup app cells for app collection view

--- AppStoreClone/CategoryCell.swift
+++ AppStoreClone/CategoryCell.swift
@@ -11,6 +11,8 @@ import UIKit
 // Custom Cell Class
 class CategoryCell: UICollectionViewCell {
   
+  let appCellId = "AppCellId"
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     
@@ -32,11 +34,51 @@ class CategoryCell: UICollectionViewCell {
   }
   
   // Create collectionView within cell
-  let appsCollectionView: UICollectionView = {
+  lazy var appsCollectionView: UICollectionView = {
     let layout = UICollectionViewFlowLayout()
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    collectionView.backgroundColor = .blue
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    return collectionView
+    let appsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    
+    appsCollectionView.delegate = self
+    appsCollectionView.dataSource = self
+    appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: self.appCellId)
+    
+    appsCollectionView.backgroundColor = .blue
+    appsCollectionView.translatesAutoresizingMaskIntoConstraints = false
+    return appsCollectionView
   }()
+}
+
+extension CategoryCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  // Number of Cells
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  // Create App Cells
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.appCellId, for: indexPath) as! AppCell
+    return cell
+  }
+  
+  // Size Cells
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 150, height: frame.height)
+  }
+  
+}
+
+class AppCell: UICollectionViewCell {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupViews()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setupViews() {
+    backgroundColor = .red
+  }
 }
