e1542b8af8f94776d08aba2c90c6006ecc5f550a
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-14 08:33:31 -0800

    Inital commit

---
+++ AppStoreClone/FeaturedAppsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Emmanuoel Haroutunian on 2/14/17.
+//  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsController: UICollectionViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
