82109e919d79f40780ebd9b995304de691d9272c
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-02-16 16:58:11 -0800

    Create Header for AppDetailController

--- AppDetailController.swift
+++ AppDetailController.swift
@@ -8,10 +8,60 @@
 
 import UIKit
 
-class AppDetailController: UICollectionViewController {
+class AppDetailController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+  
+  private let headerId = "headerId"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    collectionView?.backgroundColor = .red
+    collectionView?.register(AppDetailHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+    collectionView?.backgroundColor = .white
   }
+  
+  // Return Header View
+  override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    
+    let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+    return header
+  }
+  
+  // Header Size
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    return CGSize(width: view.frame.width, height: 170)
+  }
+}
+
+class AppDetailHeader: BaseCell {
+  
+  let imageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFill
+    return iv
+  }()
+  
+  override func setupViews(){
+    
+    backgroundColor = .blue
+  }
+  
+}
+
+class BaseCell: UICollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupViews()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setupViews() {
+    
+  }
+  
+  
 }
